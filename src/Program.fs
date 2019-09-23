2ddd613245ab4cea3c192e42f825e0c6819a001b
Thomas Boby <th.boby@gmail.com>
2019-09-23 09:15:04 +0100

    Example paket local tool

---
+++ src/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FParsec
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    // Use something random
+    eprintfn "%A" digit
+    0 // return an integer exit code
